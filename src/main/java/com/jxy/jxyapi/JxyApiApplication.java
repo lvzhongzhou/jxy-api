ff3b348ec36e84cd6e5bcc68a40d76ccb7b03554
吕中舟 <lvzhongzhou@yunzongnet.com>
2018-03-12 17:41:32 +0800

    增加基础文件

---
+++ src/main/java/com/jxy/jxyapi/JxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jxy.jxyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxyApiApplication.class, args);
+    }
+
+}
